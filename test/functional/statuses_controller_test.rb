4ad04feb62687c05bbd395f5d94dc70ffcdcb447
Ryan McCrary <rrmccrary@gmail.com>
2013-01-11 18:01:05 -0500

    Scriped status updates

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -34,8 +34,16 @@ class StatusesControllerTest < ActionController::TestCase
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
+  end
+
+  test "should create status for the current user when logged in" do
+    sign_in users(:ryan)
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:chrissy).id }
+    end
 
     assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:ryan).id
   end
 
   test "should show status" do
@@ -65,6 +73,20 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:ryan)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for the current user when logged in" do
+    sign_in users(:ryan)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:chrissy).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:ryan).id
+  end
+
+    test "should not update the status if nothing has changed" do
+    sign_in users(:ryan)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:ryan).id
   end
 
   test "should destroy status" do
